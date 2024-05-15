5ad198af24f56869ab608a6424208413faf87b1e
Antônio <antoniomarcos.souza2002@gmail.com>
2024-05-15 16:09:04 -0300

    feat: autenticando dentro da função de download

--- src/kaggle_dataset.py
+++ src/kaggle_dataset.py
@@ -2,18 +2,15 @@
 from pathlib import Path
 
 import kaggle
-from dotenv import load_dotenv
+
 from logger import create_logger
 
-load_dotenv()
-kaggle.api.authenticate()
 LOGGER = create_logger()
 
 
 # %%
 def kaggle_download(dataset: str, output_dir: Path) -> None:
-    """Função que baixa um dataset do Kaggle."""
-
+    kaggle.api.authenticate()
     kaggle.api.dataset_download_files(
         dataset=dataset,
         path=output_dir,

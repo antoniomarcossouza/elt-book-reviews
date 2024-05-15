e689aaa58a14677fa7accb72f13354c6f1e7825b
Antônio <antoniomarcos.souza2002@gmail.com>
2024-05-15 16:09:30 -0300

    docs: função kaggle_download

--- src/kaggle_dataset.py
+++ src/kaggle_dataset.py
@@ -10,6 +10,15 @@ LOGGER = create_logger()
 
 # %%
 def kaggle_download(dataset: str, output_dir: Path) -> None:
+    """Função que baixa um dataset do Kaggle.
+    
+    Parâmetros
+    ----------
+    dataset : str
+        nome do dataset a ser baixado
+    output_dir : Path
+        diretório que o arquivo será salvo
+    """
     kaggle.api.authenticate()
     kaggle.api.dataset_download_files(
         dataset=dataset,

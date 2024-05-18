ddf652ab2630c1f244278a64543e9a07a2d7e2ef
Antônio <antoniomarcos.souza2002@gmail.com>
2024-05-18 12:00:31 -0300

    feat: função pra transformar arquivo json em parquet
    todo: selecionar colunas específicas

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import urllib.request
 from pathlib import Path
 
+import duckdb
+
 from logger import create_logger
 
 LOGGER = create_logger()
@@ -23,3 +25,20 @@ def download_file(url: str, output_file: Path) -> None:
 
     urllib.request.urlretrieve(url, output_file)
     LOGGER.info(f"Arquivo {output_file} baixado.")
+
+
+def json_to_parquet(input_file: str, output_file: str) -> None:
+    """Função que converte um arquivo JSON para Parquet.
+
+    Parâmetros
+    ----------
+    input_file : str
+        caminho do arquivo JSON a ser convertido.
+    output_file : str
+        caminho do arquivo Parquet a ser criado.
+    """
+
+    duckdb.sql(
+        f"COPY(SELECT * FROM read_json_auto('{input_file}')) TO '{output_file}' (FORMAT 'parquet');"
+    )
+    LOGGER.info(f"Arquivo {output_file} criado.")

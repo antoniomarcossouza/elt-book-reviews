967708dac57f5b5e766495364ec86eb3ec89719b
Antônio <antoniomarcos.souza2002@gmail.com>
2024-06-26 17:39:01 -0300

    feat: tabela de obras

---
+++ dbt_data/models/staging/works.sql
@@ -0,0 +1,7 @@
+SELECT
+    w.`original_title` AS `title`,
+    CAST(w.`work_id` AS INT64) AS `id`,
+    SAFE_CAST(w.`original_publication_year` AS INT64)
+        AS `publication_year`
+FROM
+    {{ source('staging', 'goodreads_book_works') }} AS w
